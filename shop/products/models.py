f0a11671c81170f69b642a932d0dca6116d5b075
Сергеев Виталий <serg3evvitaly@yandex.ru>
2023-02-18 09:12:02 +0300

    the main logic of the project is implemented

--- shop/products/models.py
+++ shop/products/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Item(models.Model):
+    conv_cent_usd = 100
+    name = models.CharField(max_length=100, verbose_name='Название_товара')
+    price = models.IntegerField (default=0, verbose_name='Цена')
+    description = models.TextField(verbose_name='Описание')
+
+    class Meta:
+        verbose_name = 'Элемент'
+        verbose_name_plural = 'Элементы'
+
+    def __str__(self):
+        return self.name
+
+    def get_display_price(self):
+        return '{0:.2f}'.format(self.price/settings.CONV_CENT_USD)
--- shop/products/views.py
+++ shop/products/views.py
@@ -1,3 +1,83 @@
-from django.shortcuts import render
+import stripe
 
-# Create your views here.
+from django.conf import settings
+from django.views import View
+from django.http import JsonResponse, HttpResponse
+from django.views.generic import TemplateView
+from django.shortcuts import redirect, get_object_or_404
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
+
+from products.models import Item
+
+stripe.api_key = settings.STRIPE_SECRET_KEY
+
+
+class SuccessView(TemplateView):
+    template_name = 'success.html'
+
+
+class CancelView(TemplateView):
+    template_name = 'cancel.html'
+
+
+class IndexPageView(TemplateView):
+    template_name = "index.html"
+
+    def get_context_data(self, **kwargs):
+        item = get_object_or_404(Item, id = self.kwargs.get('id'))
+        context = super(IndexPageView, self).get_context_data(**kwargs)
+        context.update({
+            "item": item,
+            "STRIPE_PUBLIC_KEY": settings.STRIPE_PUBLIC_KEY
+        })
+        return context
+
+
+@method_decorator(csrf_exempt, name='dispatch')
+class CreateCheckoutSessionView(View):
+
+    def post(self, request, *args, **kwargs):
+        YOUR_DOMAIN = 'http://127.0.0.1:8000'
+        item = Item.objects.get(id=kwargs.get('id'))
+        session = stripe.checkout.Session.create(
+            payment_method_types=['card'],
+            line_items=[{
+                'price_data': {
+                    'currency': 'usd',
+                    'product_data': {
+                        'name': item.name,
+                    },
+                    'unit_amount': item.price,
+                },
+                'quantity': 1,
+            }],
+            mode='payment',
+            success_url=YOUR_DOMAIN +'/success',
+            cancel_url=YOUR_DOMAIN + '/cancel',
+        )
+        return redirect(session.url, code=303)
+
+
+@csrf_exempt
+def stripe_webhook(request):
+    payload = request.body
+    sig_header = request.META['HTTP_STRIPE_SIGNATURE']
+    event = None
+
+    try:
+        event = stripe.Webhook.construct_event(
+            payload, sig_header, settings.STRIPE_WEBHOOK_SECRET
+        )
+    except ValueError as e:
+        # Invalid payload
+        return HttpResponse(status=400)
+    except stripe.error.SignatureVerificationError as e:
+        # Invalid signature
+        return HttpResponse(status=400)
+
+    if event['type'] == 'checkout.session.completed':
+        session = event['data']['object']
+        print(session)
+
+    return HttpResponse(status=200)
